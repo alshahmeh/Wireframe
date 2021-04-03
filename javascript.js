ef67c95807f40416c42716fa0d8a3794883cc5fe
Ali Moihammed Alshahmeh <alshahmeh2@gmail.com>
2021-04-04 01:08:00 +0300

    some things added

---
+++ javascript.js
@@ -0,0 +1,9 @@
+confirm("please Press ok");
+alert("Hello this is my website");
+    var old=prompt("can you guess how old am I?","old");
+   if(old==27){alert("good job");}
+   else if(old>27){alert("no I am smaller")}
+   else if(old<27){alert("no I am bigger")}
+   else {alert("no Iam sorry");}
+   console.log("Hello to my page");
+   document.write("Hello this is my page");
