9444e4ed0d20ab9137c76937f94a066c59bc9b5a
Ali Moihammed Alshahmeh <alshahmeh2@gmail.com>
2021-04-04 23:38:34 +0300

    added new things

--- javascript.js
+++ javascript.js
@@ -7,3 +7,23 @@ alert("Hello this is my website");
    else {alert("no Iam sorry");}
    console.log("Hello to my page");
    document.write("Hello this is my page");
+   var car=prompt("just choose one of this cars : BMW Mercedes Daewoo Kia Heundai?");
+switch(car){
+    case 'BMW':  document.write('<img style="width: 100% "; src="https://carluxur.com/wp-content/uploads/2019/12/BMW-3-Series-car.jpg">');  break;  
+    case 'Heundai':  document.write('<img style="width: 100% "; src="https://cdn.al-ain.com/images/2020/8/03/140-210115-hyundai-ioniq-2.jpeg">'); break;
+    case 'Kia':  document.write('<img style="width: 100% "; src="https://carsalesbase.com/wp-content/uploads/2013/12/Kia-Europe-Sales.png">'); break;
+    case 'Daewoo':  document.write('<img style="width: 100% "; src="https://www.cstatic-images.com/stock/400x500/252417.png">'); break;
+    case 'Mercedes':  document.write('<img style="width: 100% "; src="https://wheelz.me/wp-content/uploads/2020/09/20C0404_063.jpg">'); break;
+    default : alert("sorry There are just these cars ");
+}
+var input=prompt("please guess how many cars in my web page",'10');
+while(input!=10){input=prompt("please guess how many cars in my web page",'10');}
+var x = myFunction(4, 3);   // Function is called, return value will end up in x
+document.write(x);
+function myFunction(a, b) {
+  for(a=1;a<b;a++){ return a * b;}
+              // Function returns the product of a and b
+}
+
+       
+   
