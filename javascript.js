5bcef8a328d883f052bacce52e1d467f5f7e19da
Ali Alshahmeh <alshahmeh2@gmail.com>
2021-04-23 00:32:51 +0300

    I fix some syntax errors

--- javascript.js
+++ javascript.js
@@ -1,17 +1,15 @@
 'use strict';
-let x=0;
+let x = 0;
 alert('Hello my name is Ali and I will play with you about guessing game');
-let yourName=prompt('So,What is your name').toLocaleLowerCase();
-function putMyOld(years){
-  let old=prompt('Nice to meet you'+' '+yourName+' '+'I will ask you 7 questions first can you guess How old am I?');
-  if(old<years){
-    alert('No I am bigger than'+' '+old);}
-  else if(old>years){alert('No I am less than'+' '+old);
+let yourName = prompt('So,What is your name').toLocaleLowerCase();
+function putMyOld(years) {
+  let old = prompt('Nice to meet you' + ' ' + yourName + ' ' + 'I will ask you 7 questions first can you guess How old am I?');
+  if (old < years) {
+    alert('No I am bigger than' + ' ' + old);
   }
-  else{alert('good job you are very lucky'); x=x+1;
-    }
+  else if (old > years) { alert('No I am less than' + ' ' + old); }
+  else { alert('good job you are very lucky'); x = x + 1; }}
 putMyOld(27);
-
 alert('can you guess where am I from, I wil show you some options just  write that?');
 function guessMyCountry(){
   let country=prompt('Jordan'+'    '+'Syria'+'    '+'Palestine').toLocaleLowerCase();
@@ -24,7 +22,6 @@ function guessMyCountry(){
     alert('no I am sorry try again'); break;
   }}
 guessMyCountry();
-
 function checkMyUniversity(myUniversity){
   let university=prompt('guess what is my universty name').toLocaleUpperCase();
   if(university===myUniversity){
@@ -32,7 +29,6 @@ function checkMyUniversity(myUniversity){
   else{
     alert('no iam sorry');}}
 checkMyUniversity('ALZARQA');
-
 function checkMySpeciality(major){
   let speciality=prompt('so can you guess my speciality');
   if(speciality===major){
@@ -40,7 +36,6 @@ function checkMySpeciality(major){
   else{
     alert('no iam sorry');}}
 checkMySpeciality('software engineering');
-
 function checkKind(yourmood){
   let kind=prompt('so'+' '+yourName+' '+' are you happy?  just say yes or no ').toLocaleLowerCase();
   if(kind===yourmood){
@@ -48,9 +43,7 @@ function checkKind(yourmood){
   else{
     alert('no iam sorry you should be happy');}}
 checkKind('yes');
-
-
-let randomNumbers=Math.random();
+let randomNumbers=Math.floor(Math.random()*10);
 function checkRandomnumber(randomNumber){
   let answer=prompt('so can you guess my lucky number ');
   let i=0;
@@ -63,7 +56,6 @@ function checkRandomnumber(randomNumber){
   if(answer===randomNumber){alert('the answer is correct good job');x=x+1;}
   else{alert('sorry the correct answer is '+randomNumber);}}
 checkRandomnumber(randomNumbers);
-
 function checkFruites(){
   let fruites=['lemon','strawberries','oranges','limes','grapefruit','blackberries'];
   let answers=prompt('can you guess one from my favourite fruites').toLocaleLowerCase();
@@ -75,5 +67,7 @@ function checkFruites(){
   }
   if(answers===fruites[0]||answers===fruites[1]||answers===fruites[2]||answers===fruites[3]||answers===fruites[4]||answers===fruites[5]){alert('the answer is correct good job');x=x+1;}
   else{alert('sorry the correct answer are :'+' '+fruites);}
+}
 checkFruites();
 alert('your score is'+' '+x+' from '+7);
+
